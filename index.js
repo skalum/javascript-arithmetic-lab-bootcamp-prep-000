c32e3a41106e743b71a1d19a316fe8e3f7a39aec
skalum <stkalum@gmail.com>
2017-09-27 22:59:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function add (a, b) {
+  return a + b
+}
+
+function subtract (a, b) {
+  return a - b
+}
+
+function multiply (a, b) {
+  return a * b
+}
+
+function divides (a, b) {
+  return a / b
+}
